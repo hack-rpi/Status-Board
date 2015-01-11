ed9f3c85b3251d451d4acbedf8aa117f5d342621
Matt Poegel <mpoegel@comcast.net>
2015-01-10 21:38:49 -0500

    mentor and volunteer registration

--- client/login.js
+++ client/login.js
@@ -18,7 +18,11 @@ if (Meteor.isClient) {
   // trim whitespace from the input
   var trimInput = function(val) {
     return val.replace(/^\s*|\s*$/g, "");
-  }
+  };
+
+  var stripPhone = function(phone) {
+    return phone;
+  };
 
 	Template.login.events({
     'submit #login-form' : function(e, t){
@@ -107,8 +111,28 @@ if (Meteor.isClient) {
     },
     // -------------------------------------------------------------------------
     'click #reg-mentor-page1-next': function(e) {
-      $("#reg-mentor-page1").toggle("slide");
-      $("#reg-mentor-page2").toggle("slide");
+      // check input on page 1
+      var fname = trimInput($("#reg-mentor-fname").val()),
+          lname = trimInput($("#reg-mentor-lname").val()),
+          affil = trimInput($("#reg-mentor-affiliation").val()),
+          phone = trimInput($("#reg-mentor-phone").val()),
+          email = trimInput($("#reg-mentor-email").val()),
+          pass1 = trimInput($("#reg-mentor-pass1").val()),
+          pass2 = trimInput($("#reg-mentor-pass2").val());
+      if (!fname || !lname || !affil || !phone || !email || !pass1 || !pass2)
+        Session.set("displayMessage", "Field Required & One or more required fields are empty");
+      else if (!stripPhone(phone))
+        Session.set("displayMessage", "Error & Invalid phone number");
+      else if (!isValidEmail(email))
+        Session.set("displayMessage", "Error & Invalid email");
+      else if (!isValidPassword(pass1))
+        Session.set("displayMessage", "Error & Password must be at least 6 characters");
+      else if (pass1 != pass2)
+        Session.set("displayMessage", "Error & Passwords do not match");
+      else {
+        $("#reg-mentor-page1").toggle("slide");
+        $("#reg-mentor-page2").toggle("slide");
+      }
     },
     'click #reg-mentor-page2-prev': function(e) {
       $("#reg-mentor-page2").toggle("slide");
@@ -130,17 +154,147 @@ if (Meteor.isClient) {
       $("#reg-mentor-page4").toggle("slide");
       $("#reg-mentor-page3").toggle("slide");
     },
+    'click #reg-mentor-submit': function(e,t) {
+      e.preventDefault();
+      var fname = trimInput($("#reg-mentor-fname").val()),
+          lname = trimInput($("#reg-mentor-lname").val()),
+          affil = trimInput($("#reg-mentor-affiliation").val()),
+          phone = trimInput($("#reg-mentor-phone").val()),
+          email = trimInput($("#reg-mentor-email").val()),
+          pass1 = trimInput($("#reg-mentor-pass1").val()),
+          pass2 = trimInput($("#reg-mentor-pass2").val()),
+          languages = [],
+          frameworks = [],
+          apis = [];
+
+      $("#reg-mentor-languages input:checked").each(function() {
+        languages.push(this.name);
+      });
+      $("#reg-mentor-frameworks input:checked").each(function() {
+        frameworks.push(this.name);
+      });
+      $("#reg-mentor-apis input:checked").each(function() {
+        apis.push(this.name);
+      });
+
+      phone = stripPhone(phone);
+
+      Accounts.createUser({ email: email,
+                            password: pass1,
+                            profile: {
+                              first_name: fname,
+                              last_name: lname,
+                              affiliation: affil,
+                              phone: phone,
+                              languages: languages,
+                              frameworks: frameworks,
+                              apis: apis,
+                            },
+        }, function(err) {
+        if (err) {
+          Session.set("displayMessage", "Error & Something went wrong. Please try again later");
+        }
+        else {
+          // success
+          Router.go("/");
+        }
+      });
+      return false;
+    },
     // -------------------------------------------------------------------------
     'click #reg-volunteer-page1-next': function(e) {
-      $("#reg-volunteer-page1").toggle("slide");
-      $("#reg-volunteer-page2").toggle("slide");
+      var fname = trimInput($("#reg-volunteer-fname").val()),
+          lname = trimInput($("#reg-volunteer-lname").val()),
+          phone = trimInput($("#reg-volunteer-phone").val()),
+          email = trimInput($("#reg-volunteer-email").val()),
+          pass1 = trimInput($("#reg-volunteer-pass1").val()),
+          pass2 = trimInput($("#reg-volunteer-pass2").val());
+      if (!fname || !lname || !phone || !email || !pass1 || !pass2)
+        Session.set("displayMessage", "Field Required & One or more required fields are empty");
+      else if (!stripPhone(phone))
+        Session.set("displayMessage", "Error & Invalid phone number");
+      else if (!isValidEmail(email))
+        Session.set("displayMessage", "Error & Invalid email");
+      else if (!isValidPassword(pass1))
+        Session.set("displayMessage", "Error & Password must be at least 6 characters");
+      else if (pass1 != pass2)
+        Session.set("displayMessage", "Error & Passwords do not match");
+      else {
+        $("#reg-volunteer-page1").toggle("slide");
+        $("#reg-volunteer-page2").toggle("slide");
+      }
     },
     'click #reg-volunteer-page2-prev': function(e) {
       $("#reg-volunteer-page2").toggle("slide");
       $("#reg-volunteer-page1").toggle("slide");
     },
     'click .time-grid-box': function(e) {
-      $("#"+e.currentTarget.id).toggleClass("time-grid-box-selected");
+      $(e.currentTarget).toggleClass("time-grid-box-selected");
+      if ($(e.currentTarget).attr('is_selected') == 'true')
+        $(e.currentTarget).attr('is_selected', 'false');
+      else
+        $(e.currentTarget).attr('is_selected', 'true');
+    },
+    'click #reg-volunteer-submit': function(e) {
+      e.preventDefault();
+      var fname = trimInput($("#reg-volunteer-fname").val()),
+          lname = trimInput($("#reg-volunteer-lname").val()),
+          phone = trimInput($("#reg-volunteer-phone").val()),
+          email = trimInput($("#reg-volunteer-email").val()),
+          pass1 = trimInput($("#reg-volunteer-pass1").val()),
+          pass2 = trimInput($("#reg-volunteer-pass2").val()),
+
+      phone = stripPhone(phone);
+
+      var in_block = false,
+          block_start = null,
+          block_end = null,
+          blocks = [];
+      $(".time-grid-box").each(function() {
+        if ($(this).attr('is_selected') == 'true') {
+          if (!in_block) {
+            in_block = true;
+            block_start = $(this).attr('date');
+          }
+          block_end = $(this).attr('date');
+        }
+        else if (in_block) {
+          in_block = false;
+          block_end = new Date(block_end);
+          block_end.setHours(block_end.getHours()+1);
+          blocks.push({start:new Date(block_start), end:block_end});
+        }
+      });
+      if (in_block) {
+        block_end = new Date(block_end);
+        block_end.setHours(block_end.getHours()+1);
+        blocks.push({start:new Date(block_start), end:block_end});
+      }
+
+      if (blocks.length == 0) {
+        Session.set("displayMessage", "Field Required & Please select the hours you are available");
+        return false;
+      }
+
+      Accounts.createUser({ email: email,
+                            password: pass1,
+                            profile: {
+                              first_name: fname,
+                              last_name: lname,
+                              phone: phone,
+                              blocks: blocks,
+                            },
+        }, function(err) {
+        if (err) {
+          Session.set("displayMessage", "Error & Something went wrong. Please try again later");
+        }
+        else {
+          // success
+          Router.go("/");
+        }
+      });
+
+      return false;
     },
   });
 
@@ -160,6 +314,8 @@ if (Meteor.isClient) {
       $("<div>", {
         id: 'time-grid-box-' + box_count,
         'class': 'time-grid-box',
+        date: s,
+        is_selected: 'false',
         text: s.getHours() + ':00',
       }).appendTo("#volunteer-time-grid");
       s.setHours(s.getHours()+1);
