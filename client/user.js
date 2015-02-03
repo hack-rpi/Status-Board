6ea58df60a222514014021dc82835719b5e054bc
Matt <mpoegel@comcast.net>
2015-02-03 22:02:51 +0000

    user-profile basics

--- client/user.js
+++ client/user.js
@@ -7,6 +7,28 @@ if (Meteor.isClient) {
     $("#user-profile-btn").addClass("active");
   };
 
+  var s_username = "";
+  var s_userID = "";
+  var s_realname = "";
+  var s_projectname = "";
+  var s_location = "";
+
+  Tracker.autorun(function(){
+    // called automatically whenever the session variable changes
+    var selectedUserName = Session.get("selectedUserName");
+    Meteor.subscribe("userData");
+    var s_user = Meteor.users().find( {username : selectedUserName} ).fetch()[0];
+    if (s_user) {
+      s_username = selectedUserName;
+      s_userID = s_user._id;
+      if (s_user.profile) {
+        s_realname = s_user.profile.real_name;
+        s_projectname = s_user.profile.project_name;
+        s_location = s_user.profile.location;
+      }
+    }
+  });
+
 
   Template.user.helpers({
     user_page: function() {
@@ -42,8 +64,29 @@ if (Meteor.isClient) {
 // =============================================================================
 
   Template.user_profile.helpers({
-    currentUserName: function() {
-      return Session.get('selectedUserName');
+    userName: function() { return s_username; },
+    realName: function() { return s_realname; },
+    projectName: function() { return s_projectname; },
+    location: function() { return s_location; },
+
+    editable: function() {
+      if (Meteor.user)
+        return s_userID == Meteor.user._id;
+    },
+    editActive: function() {
+      return user-profile-edit;
+    },
+  });
+
+  var user-profile-edit = false;
+
+  Template.user_profile.events({
+    'click #user-profile-edit-btn': function() {
+      user-profile-edit = true;
+    },
+    'click #user-profile-save-btn': function() {
+      // check edits and save to db
+      user-profile-edit = false;
     },
   });
 
