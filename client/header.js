607b5798e3da59f13c659aa36b6862dc5c6e8552
Matt Poegel <mpoegel@comcast.net>
2015-02-08 23:31:07 +0000

    redesign with feedback from CDR. logout button broken

--- client/header.js
+++ client/header.js
@@ -8,12 +8,29 @@ if (Meteor.isClient) {
 		$('#'+page_name).addClass('nav-button-selected');
 	});
 
+	$(document).click(function() {
+		if ($(".user-popup").is(":visible"))
+			$(".user-popup").fadeToggle(200);
+	});
+
 	Template.header.events({
-		'click .nav-button-logout': function() {
+		'click .nav-button-logout': function(e) {
 			Meteor.logout();
+			// e.stopPropagation();
 		},
-		'click #nav-user': function() {
-			Session.set("selectedUserId", Meteor.userId());
+		'click #nav-user': function(e) {
+			var moveLeft = 20;
+			var moveDown = 10;
+			var p_position = $("#nav-user").position()
+			$(".user-popup").fadeToggle(200)
+				.css("top", p_position.top + $("#nav-user").height() +moveDown)
+				.css("left", p_position.left + $("#nav-user").width() +moveLeft)
+				.appendTo("body");
+			e.stopPropagation();
+		},
+		'click .user-popup-btn': function(e) {
+			$(".user-popup").fadeToggle(200);
+			// e.stopPropagation();
 		}
 	});
 
