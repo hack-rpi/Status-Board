4f9ef5280f902c12fb9f6a947d8bf713d172a75a
Matt Poegel <mpoegel@comcast.net>
2015-04-27 00:18:34 -0400

    successfully captured POST request from Github!

--- server/api.js
+++ server/api.js
@@ -14,11 +14,76 @@ Restivus.addRoute('CommitMessages', { authRequired: true}, {
 	},
 	post: {
 		action: function() {
-			console.log("POOOOOST!");
-			return {
-				status: 'success',
-				data: []
-			};
+			var commits = this.request.body.commits;
+			if (commits.length === 0) {
+				return {
+					statusCode: 400,
+					body: {
+						status: 'Fail',
+						message: 'No commit messages found.'
+					}
+				};
+			}
+			var success_count = 0;
+			for (var i = 0; i < commits.length; i++) {
+				if (! commits[i].distinct ||
+					CommitMessages.findOne({_id:commits[i].id})) {
+					continue;
+				}
+				var v_date = Meteor.call('validateDate', commits[i].timestamp);
+				if (CommitMessages.insert({
+					_id: commits[i].id,
+					text: commits[i].message,
+					url: commits[i].url,
+					date: v_date,
+					fdate: Meteor.call('formatDateTime', v_date),
+					author: {
+						name: commits[i].author.name,
+						email: commits[i].author.email,
+						username: commits[i].author.username
+					},
+					repo: {
+						id: this.request.body.repository.id,
+						name: this.request.body.repository.name,
+						full_name: this.request.body.repository.full_name,
+						owner: this.request.body.repository.owner,
+						description: this.request.body.repository.description,
+						url: this.request.body.repository.html_url,
+						homepage: this.request.body.repository.homepage,
+						language: this.request.body.repository.language
+					}
+				})) {
+					success_count++;
+				}
+			}
+			if (success_count == 0) {
+				return {
+					statusCode: 401,
+					body: {
+						status: 'Fail',
+						message: 'You are not authorized to add commit messages.'
+					}
+				};
+			}
+			else if (success_count < commits.length) {
+				return {
+					statusCode: 206,
+					body: {
+						status: 'Partial Success',
+						message: 'Added ' + success_count + ' of ' + commits.length +
+							' commit messages successfully.'
+					}
+				};
+			}
+			else {
+				return {
+					statusCode: 201,
+					body: {
+						status: 'Success',
+						message: 'Commit messages successfully added to the database.'
+					}
+				};
+			}
 		}
 	}
 });
