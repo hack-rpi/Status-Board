312252946bb3f71fd7ce9b67536aa2f8880c2b10
Matt Poegel <mpoegel@comcast.net>
2015-10-05 13:12:43 -0400

    :squirrel: fixed collection syntax. removed publish call for deprecated mentor collection

--- server/collections/publications.js
+++ server/collections/publications.js
@@ -2,7 +2,6 @@
 Meteor.publish("CommitMessages", function() { return CommitMessages.find(); });
 Meteor.publish("RepositoryList", function() { return RepositoryList.find(); });
 Meteor.publish("Announcements", function() {  return Announcements.find(); });
-Meteor.publish("Mentors", function() {        return Mentors.find(); });
 Meteor.publish("MentorQueue", function() {    return MentorQueue.find(); });
 // users can view all of their own data but only the profiles and usernames
 //  of other users
