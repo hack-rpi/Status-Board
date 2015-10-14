95136a6f83965cf054022f9a884d1251de21bc53
Matt Poegel <mpoegel@comcast.net>
2015-10-14 06:15:16 -0400

    updated user profile for new fields. locked hacker dashboard until the event

--- client/user/profile.js
+++ client/user/profile.js
@@ -21,6 +21,11 @@ Template.user_profile.helpers({
       return Meteor.user().profile.phone;
     else return "";
   },
+  location: function() {
+    if (Meteor.userId())
+      return Meteor.user().profile.location;
+    else return '';
+  },
   school: function() {
     if (Meteor.userId())
       return Meteor.user().profile.school;
@@ -36,10 +41,37 @@ Template.user_profile.helpers({
       return Meteor.user().profile.diet;
     else return [];
   },
+  resume: function() {
+    if (Meteor.userId())
+      return Meteor.user().profile.resume;
+    else return '';
+  },
   editActive: function() {
     user_profile_edit_dep.depend();
     return user_profile_edit;
   },
+  allBuses: function() {
+    if (Meteor.userId()) {
+      return _.map(Meteor.settings.public.buses, function(route) {
+        return {
+          route: route,
+          selected: Meteor.user().profile.bus === route
+        }
+      });
+    }
+    else return Meteor.settings.public.buses;
+  },
+  allDiets: function() {
+    if (Meteor.userId()) {
+      return _.map(Meteor.settings.public.diet, function(name) {
+        return {
+          name: name,
+          selected: _.contains(Meteor.user().profile.diet, name)
+        }
+      });
+    }
+    else return Meteor.settings.public.diet;
+  }
 });
 
 var user_profile_edit = false;
@@ -51,31 +83,77 @@ Template.user_profile.events({
     user_profile_edit = true;
     user_profile_edit_dep.changed();
   },
+  'change #user-profile input[name="resume"]': function() {
+    var resume_file = $('#user-profile input[name="resume"]')[0].files[0];
+    if (! resume_file) 
+      return;
+    else if (resume_file.type !== 'application/pdf') {
+      Session.set('displayMessage', {
+        title: 'Resume Error',
+        body: 'Resume upload must be a PDF.'
+      });
+      return;
+    } 
+    else if (resume_file.size / 1024 > 1024) {
+      Session.set('displayMessage', {
+        title: 'Resume Error',
+        body: 'Maximum resume file size is 1MB.'
+      });
+      return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(event) {
+      var binary_data = new Uint8Array(reader.result);
+      Meteor.users.update({ _id: Meteor.userId() }, {
+        $set: {
+          'profile.resume': binary_data
+        }
+      });
+    }
+    reader.readAsArrayBuffer(resume_file);
+  },
   'click #user-profile-save-btn': function(e, t) {
     // check edits and save to db
     e.preventDefault();
     Meteor.subscribe("userData");
     var old_profile = Meteor.user().profile;
 
-    var new_name = t.find("#UPedit-name").value;
-    var new_affiliation = t.find("#UPedit-affiliation").value;
-    var new_phone = t.find("#UPedit-phone").value;
-    var new_location = t.find("#UPedit-location").value;
+    var new_name = t.find("#UPedit-name").value,
+        new_affiliation = t.find("#UPedit-affiliation").value,
+        new_phone = t.find("#UPedit-phone").value,
+        new_location = t.find("#UPedit-location").value,
+        new_school = t.find('#UPedit-school').value,
+        new_bus = $('.bus-selection input:checked').attr('value') || '',
+        new_diet = [];
+        
+    $('.diet-selection input:checked').each(function() {
+      new_diet.push(this.value);
+    });
 
     if (Meteor.users.update({ "_id": Meteor.userId() }, {
         $set: {
           "profile.name": new_name,
           "profile.affiliation": new_affiliation,
           "profile.phone": new_phone,
-          "profile.school": new_location
+          'profile.location': new_location,
+          "profile.school": new_school,
+          'profile.bus': new_bus,
+          'profile.diet': new_diet
         }
     })) {
       // data save successfully
-      Session.set("displayMessage", {title: "Success", body: "Data saved successfully!"});
+      Session.set("displayMessage", {
+        title: "Success", 
+        body: "Data saved successfully!"
+      });
     }
     else {
       // data failed to save
-      Session.set("displayMessage", {title: "Error", body: "Something went wrong saving the data! You may not have permission to perform this action."});
+      Session.set("displayMessage", {
+        title: "Error", 
+        body: 'Something went wrong saving the data! You may not have ' +
+          'permission to perform this action.'
+        });
     }
 
     user_profile_edit = false;
