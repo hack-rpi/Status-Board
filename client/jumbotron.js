15c03290ff88184253695e7b88935c7717b69664
Ariel Lee <leea10@rpi.edu>
2015-10-20 00:10:25 -0400

    Scroll through announcements with jumbotron arrows Sort announcements in descending order by start time Slight restyling of announcement text Turn jumbotron stylesheet from CSS to LESS

--- client/jumbotron.js
+++ client/jumbotron.js
@@ -1,7 +1,9 @@
+Session.setDefault('currentAnnouncement', 0);
+
 Template.jumbotron.helpers({
   showJumbo: function() {
     Meteor.subscribe("Announcements");
-    if (Announcements.find({visible:true}).count() == 0) {
+    if (Announcements.find({visible:true}).count() === 0) {
       return false;
     }
     else {
@@ -10,7 +12,25 @@ Template.jumbotron.helpers({
   },
   announcements: function() {
     Meteor.subscribe("Announcements");
-    // var d = new Date() // now
-    return Announcements.find({ visible:true }).fetch();
+    return Announcements.find({ visible:true }, {sort: {startTime: -1}}).fetch();
   },
+  showAnnouncement: function(index) {
+    return Session.get('currentAnnouncement') === index;
+  }
+});
+
+Template.jumbotron.events({
+  'click .next-announcement': function(e) {
+    var nextAnnouncement = Session.get('currentAnnouncement') + 1;
+    var numAnnouncements = Announcements.find({visible:true}).count()
+    if(nextAnnouncement < numAnnouncements) {
+      Session.set('currentAnnouncement', nextAnnouncement);
+    }
+  },
+  'click .prev-announcement': function(e) {
+    var prevAnnouncement = Session.get('currentAnnouncement') - 1;
+    if(prevAnnouncement >= 0) {
+      Session.set('currentAnnouncement', prevAnnouncement);
+    }
+  }
 });
